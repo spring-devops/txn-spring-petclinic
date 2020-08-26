f2986ab92992ea4752ef9d607244cf2224a322dd
Spring DevOps <harman009-springdevops@gmail.command>
2020-08-25 21:57:26 -0400

    Initial Project Add

---
+++ src/main/java/testapps/springframework/txnspringpetclinic/TxnSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package testapps.springframework.txnspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnSpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxnSpringPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/testapps/springframework/txnspringpetclinic/TxnSpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package testapps.springframework.txnspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnSpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
