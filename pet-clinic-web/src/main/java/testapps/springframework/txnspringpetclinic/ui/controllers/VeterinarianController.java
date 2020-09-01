a3a2966614b14168970ef00ae9cd3a663d4e65cb
Spring DevOps <harman009-springdevops@gmail.command>
2020-09-01 14:29:39 -0400

    Added Owner Veterinarian Listing. Closes #19

--- pet-clinic-web/src/main/java/testapps/springframework/txnspringpetclinic/ui/controllers/VeterinarianController.java
+++ pet-clinic-web/src/main/java/testapps/springframework/txnspringpetclinic/ui/controllers/VeterinarianController.java
@@ -1,8 +1,9 @@
 package testapps.springframework.txnspringpetclinic.ui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import testapps.springframework.txnspringpetclinic.data.services.VeterinarianCrudService;
 import testapps.springframework.txnspringpetclinic.ui.resolvers.ThymeLeafUrlService;
 
 @Controller
@@ -10,12 +11,16 @@ public class VeterinarianController {
 
     private final ThymeLeafUrlService thymeLeafUrlService;
 
-    public VeterinarianController(ThymeLeafUrlService thymeLeafUrlService) {
+    public VeterinarianController(ThymeLeafUrlService thymeLeafUrlService, VeterinarianCrudService veterinarianCrudService) {
         this.thymeLeafUrlService = thymeLeafUrlService;
+        this.veterinarianCrudService = veterinarianCrudService;
     }
 
+    private final VeterinarianCrudService veterinarianCrudService;
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html", "/vets/index.aspx"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", veterinarianCrudService.findAll());
         return this.thymeLeafUrlService.getUrl("vets/index") ;
     }
 }
