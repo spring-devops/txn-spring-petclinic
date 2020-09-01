d0b753e484c3e64bb5f27f0e605ba20dde4903d4
Spring DevOps <harman009-springdevops@gmail.command>
2020-09-01 14:25:37 -0400

    Added Owner Listing. Closes #20

--- pet-clinic-web/src/main/java/testapps/springframework/txnspringpetclinic/ui/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/testapps/springframework/txnspringpetclinic/ui/controllers/OwnerController.java
@@ -1,7 +1,10 @@
 package testapps.springframework.txnspringpetclinic.ui.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import testapps.springframework.txnspringpetclinic.data.services.OwnerCrudService;
+import testapps.springframework.txnspringpetclinic.data.services.OwnerServiceMap;
 import testapps.springframework.txnspringpetclinic.ui.resolvers.ThymeLeafUrlService;
 
 @RequestMapping("/owners")
@@ -10,12 +13,17 @@ public class OwnerController {
 
     private final ThymeLeafUrlService thymeLeafUrlService;
 
-    public OwnerController(ThymeLeafUrlService thymeLeafUrlService) {
+    public OwnerController(ThymeLeafUrlService thymeLeafUrlService, OwnerServiceMap ownerServiceMap) {
         this.thymeLeafUrlService = thymeLeafUrlService;
+        this.ownerCrudService = ownerServiceMap;
     }
 
+    private final OwnerCrudService ownerCrudService;
+
     @RequestMapping({"", "/", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerCrudService.findAll());
         return thymeLeafUrlService.getUrl("owners/index.html");
     }
 }
