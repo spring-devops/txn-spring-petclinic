1064aad4d43a468bc596c3780223e8e6d87777f7
Spring DevOps <harman009-springdevops@gmail.command>
2020-09-03 17:13:22 -0400

    Added Contact Info to Owner. Closes #35

--- pet-clinic-data/src/main/java/testapps/springframework/txnspringpetclinic/data/model/Owner.java
+++ pet-clinic-data/src/main/java/testapps/springframework/txnspringpetclinic/data/model/Owner.java
@@ -5,6 +5,9 @@ import java.util.Set;
 public class Owner extends Person{
 
     private Set<Pet> pets;
+    private String addressLine1;
+    private String city;
+    private String telephone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -12,5 +15,29 @@ public class Owner extends Person{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
